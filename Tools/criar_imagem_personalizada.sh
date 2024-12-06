296b6fcdc265f0743f1484b6e2aa6d104edd4532
fctassinari <ftassinari@yahoo.com.br>
2024-12-06 08:44:05 -0300

    First Commit

---
+++ Tools/criar_imagem_personalizada.sh
@@ -0,0 +1,9 @@
+podman rmi default-route-openshift-image-registry.apps.cluster-pwtfx.dynamic.redhatworkshops.io/openshift/smt-pipeline-tools:1.0
+
+podman build -f Dockerfile --tag=default-route-openshift-image-registry.apps.cluster-pwtfx.dynamic.redhatworkshops.io/openshift/smt-pipeline-tools:1.0
+
+podman login -u admin  -p $(oc whoami -t) default-route-openshift-image-registry.apps.cluster-pwtfx.dynamic.redhatworkshops.io --tls-verify=false
+
+podman push default-route-openshift-image-registry.apps.cluster-pwtfx.dynamic.redhatworkshops.io/openshift/smt-pipeline-tools:1.0 --tls-verify=false
+
+oc get is -n openshift | grep smt
